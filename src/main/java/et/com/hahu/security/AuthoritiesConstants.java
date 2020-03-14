51a10090c9fc27507379ab4304001361e8e8e1a4
hailemaryam <hailemaryam123456!!!>
2020-03-14 21:29:17 +0300

    SCHOOL, TEACHER and PARENT role added

--- src/main/java/et/com/hahu/security/AuthoritiesConstants.java
+++ src/main/java/et/com/hahu/security/AuthoritiesConstants.java
@@ -9,6 +9,12 @@ public final class AuthoritiesConstants {
 
     public static final String USER = "ROLE_USER";
 
+    public static final String SCHOOL = "ROLE_SCHOOL";
+
+    public static final String TEACHER = "ROLE_TEACHER";
+
+    public static final String PARENT = "ROLE_PARENT";
+
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
     private AuthoritiesConstants() {
