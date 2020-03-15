25d1054b8a3de821225f933a22f1c7ba26d15449
hailemaryam <hailemaryam123456!!!>
2020-03-15 11:02:09 +0300

    Home page Reverted to default. changes will be added at End

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,37 +1,20 @@
-import { Component, OnInit, OnDestroy, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 
+import { LoginModalService } from 'app/core/login/login-modal.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
-import { LoginService } from 'app/core/login/login.service';
-import { Router } from '@angular/router';
-import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'jhi-home',
   templateUrl: './home.component.html',
   styleUrls: ['home.scss']
 })
-export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
+export class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
   authSubscription?: Subscription;
-  @ViewChild('username', { static: false })
-  username?: ElementRef;
 
-  authenticationError = false;
-
-  loginForm = this.fb.group({
-    username: [''],
-    password: [''],
-    rememberMe: [false]
-  });
-
-  constructor(
-    private loginService: LoginService,
-    private router: Router,
-    private fb: FormBuilder,
-    private accountService: AccountService
-  ) {}
+  constructor(private accountService: AccountService, private loginModalService: LoginModalService) {}
 
   ngOnInit(): void {
     this.authSubscription = this.accountService.getAuthenticationState().subscribe(account => (this.account = account));
@@ -40,52 +23,14 @@ export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
   isAuthenticated(): boolean {
     return this.accountService.isAuthenticated();
   }
+
+  login(): void {
+    this.loginModalService.open();
+  }
+
   ngOnDestroy(): void {
     if (this.authSubscription) {
       this.authSubscription.unsubscribe();
     }
-  }
-  ngAfterViewInit(): void {
-    if (this.username) {
-      this.username.nativeElement.focus();
-    }
-  }
-
-  cancel(): void {
-    this.authenticationError = false;
-    this.loginForm.patchValue({
-      username: '',
-      password: ''
-    });
-  }
-
-  login(): void {
-    this.loginService
-      .login({
-        username: this.loginForm.get('username')!.value,
-        password: this.loginForm.get('password')!.value,
-        rememberMe: this.loginForm.get('rememberMe')!.value
-      })
-      .subscribe(
-        () => {
-          this.authenticationError = false;
-          if (
-            this.router.url === '/account/register' ||
-            this.router.url.startsWith('/account/activate') ||
-            this.router.url.startsWith('/account/reset/')
-          ) {
-            this.router.navigate(['']);
-          }
-        },
-        () => (this.authenticationError = true)
-      );
-  }
-
-  register(): void {
-    this.router.navigate(['/account/register']);
-  }
-
-  requestResetPassword(): void {
-    this.router.navigate(['/account/reset', 'request']);
   }
 }
